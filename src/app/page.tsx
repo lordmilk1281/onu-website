2aace96e629293f4b6c3a819765784129176c5dd
abu <55664644+lordmilk1281@users.noreply.github.com>
2024-02-04 08:31:19 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="mb-12 space-y-6">
       <FilterTypes />
 
-      <Container className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <Container className="xl:max-w-[90vw] grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 3xl:grid-cols-5">
         {units.map((unit, index) => (
           <UnitCard key={index} {...unit} />
         ))}
